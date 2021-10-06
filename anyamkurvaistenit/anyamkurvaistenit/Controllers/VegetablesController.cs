d7e34aee03804f4edd5cae35b792cab6b913a0a6
sooskristof123 <sooskristof123@gmail.com>
2021-10-06 15:55:22 +0200

    comm

---
+++ anyamkurvaistenit/anyamkurvaistenit/Controllers/VegetablesController.cs
@@ -0,0 +1,48 @@
+﻿using anyamkurvaistenit.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace anyamkurvaistenit.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VegetablesController : ControllerBase
+    {
+        private static List<Vegetables> VegetablesList { get; set; } = new List<Vegetables>();
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Vegetables vegetable) {
+            VegetablesList.Add(vegetable);
+            return Created("OK", vegetable);
+        }
+
+        [HttpGet]
+        public IEnumerable<Vegetables> Get() {
+            return VegetablesList;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id) {
+            var item = VegetablesList.First(x => x.id == id);
+
+            if (item == null) {
+                return NotFound();
+            }
+            return new JsonResult(item);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) {
+            var item = VegetablesList.First(x => x.id == id);
+            if (item == null) {
+                return NotFound();
+            }
+            VegetablesList.Remove(item);
+            return Ok();
+        }
+    }
+}
---
+++ anyamkurvaistenit/anyamkurvaistenit/Model/Vegetables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace anyamkurvaistenit.Model
+{
+    public class Vegetables
+    {
+        public int id { get; set; }
+        public String name { get; set; }
+    }
+}
